328323629549b6325bae34fbb59ca6d27afee232
Yves Martial Boah <yvesmartialboah@gmail.com>
2020-03-17 13:53:36 +0200

    Mise en place du dashboard admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,66 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.app-sem') @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        <br>
+        <div class="ui grid">
+            <div class="three wide column">
+                <div class="ui cards">
+                    <div class="card">
+                        <div class="content">
+                            <i class="right floated pencil alternate icon"></i>
+                            <div class="header">Rédacteurs </div>
+                            <div class="meta">Membres du journal l'<b><i>Agitelien</i></b> </div>
+                            <div class="description"><i class="eye icon"></i> <b>15</b> </div>
                         </div>
-                    @endif
-
-                    You are logged in!
-
-                    @membre 
-    
-                        Je suis Un admin
-
-                    @endmembre
+                    </div>
                 </div>
             </div>
+
+            <div class="three wide column">
+                <div class="ui cards">
+                   <div class="card">
+                        <div class="content">
+                            <i class="right floated newspaper icon"></i>
+                            <div class="header">Publications </div>
+                            <div class="meta">Articles <b><i>publiés</i></b> </div>
+                            <div class="description"><i class="eye icon"></i> <b>25</b> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="three wide column">
+                <div class="ui cards">
+                    <div class="card">
+                        <div class="content">
+                            <i class="right floated video icon"></i>
+                            <i class="right floated microphone icon"></i>
+                            <div class="header">Interviews </div>
+                            <div class="meta">Adventure sur le <b><i>terrain</i></b> </div>
+                            <div class="description"><i class="eye icon"></i> <b>45</b> </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+
+            <div class="three wide column">
+                <div class="ui cards">
+                    <div class="card">
+                        <div class="content">
+                            <i class="right floated dollar sign icon"></i>
+                            <div class="header">Don </div>
+                            <div class="meta">Reçu sur la plateforme l'<b><i>Agitelien</i></b> </div>
+                            <div class="description"><i class="eye icon"></i> <b>1.500.000</b><i class="dollar sign icon"></i> </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-    </div>
+
+
 </div>
-@endsection
+@endsection @membre
+
+<!-- Je suis Un admin -->
+
+@endmembre
---
+++ resources/views/layouts/app-sem.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Acceuil</title>
+    <!-- <script src="https://kit.fontawesome.com/98637d73ea.js" crossorigin="anonymous"></script> -->
+    <link rel="stylesheet" href="{{asset('semantic-ui/semantic.min.css')}}">
+    <link rel="stylesheet" href="{{asset('semantic-ui/icon.css')}}">
+</head>
+
+<body>
+
+	<div class="ui container inverted top attached menu">
+	  <div class="right menu">
+	    <div class="item">
+	      <div class="ui icon input">
+	        <input type="text" placeholder="Rechercher...">
+	        <i class="search link icon"></i>
+	      </div>
+	    </div>
+	    <!-- <a class="ui item">---- </a> -->
+	  </div>
+	</div>
+
+    <div class="ui left demo vertical inverted visible sidebar labeled icon menu">
+	  <a class="item">
+	    <i class="home icon"></i>
+	    Accueil
+	  </a>
+	  <a class="item">
+	    <i class="pencil alternate icon"></i>
+	    Ajouter un rédacteur
+	  </a> 
+	  <a class="item">
+	    <i class="newspaper icon"></i>
+	    Ajouter un nouvel article
+	  </a> 
+	  <a class="item">
+	    <p><i class="microphone icon"></i>
+	    <i class="video icon"></i></p>
+	    Ajouter une interview
+	  </a>
+	  <a class="item">
+	    <i class="close icon"></i>
+	    Déconnexion
+	  </a>
+	</div>
+
+    <div class="dimmed pusher">
+        <!-- Site content !-->
+        @yield('content')
+    </div>
+
+    <script src="{{asset('jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('semantic-ui/semantic.js')}}"></script>
+    <script>
+    	function menu(){
+	    		$('.ui.labeled.icon.sidebar')
+				  .sidebar('toggle')
+				;	
+    	}
+    </script>
+</body>
+
+</html>
